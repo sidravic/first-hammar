be872cc124cd86c60930e1a5df9dec057fe8788a
SR <sid.ravichandran@gmail.com>
2019-11-11 18:53:36 +0530

    Added subnet zone

--- staging/aws/create-swarm-manager.sh
+++ staging/aws/create-swarm-manager.sh
@@ -15,9 +15,20 @@ function create_manager_nodes(){
                           --amazonec2-tags swarm_manager,true,environment,staging \
                           --amazonec2-volume-type gp2 \
                           --amazonec2-vpc-id $DEFAULT_VPC_ID \
-                          --amazonec2-ssh-keypath $DEFAULT_KEY_PATH "$node_name" 
+                          --amazonec2-ssh-keypath $DEFAULT_KEY_PATH \
+                          --amazonec2-zone $subnet_zone "$node_name" 
 
 
+}
+
+
+function find_subnet_zone(){
+    if [ $(($i % 2)) -eq 0 ];
+    then
+        subnet_zone="c"
+    else
+        subnet_zone="b"
+    fi
 }
 
 
@@ -27,6 +38,8 @@ function create_3_manager_nodes(){
     
     for i in 1 2 3;
     do 
+        find_subnet_zone
+        echo $subnet_zone
         node_name="vm${i}"
         echo "Creating swarm manager nodes $node_name"
         create_manager_nodes
