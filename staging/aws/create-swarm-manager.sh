e8936c7619669e622a68d39d8824be4bdb8bab4b
SR <sid.ravichandran@gmail.com>
2019-11-11 17:36:19 +0530

    Manager nodes with names

--- staging/aws/create-swarm-manager.sh
+++ staging/aws/create-swarm-manager.sh
@@ -14,16 +14,20 @@ function create_manager_nodes(){
                           --amazonec2-security-group $swarm_security_group_id \
                           --amazonec2-tags swarm_manager,true,environment,staging
                           --amazonec2-volume-type gp2
-                          --amazonec2-vpc-id $DEFAULT_VPC_ID
+                          --amazonec2-vpc-id $DEFAULT_VPC_ID "$node_name"
 
 
 }
 
 
 function create_3_manager_nodes(){
+    echo "Getting security group for swarm"
+    swarm_security_group_id=get_swarm_security_group_id
+    
     for i in 1 2 3;
     do 
-        swarm_security_group_id=get_swarm_security_group_id
-        create_manager_node
+        node_name="vm${i}"
+        echo "Creating swarm manager nodes $node_name"
+        create_manager_nodes
     done
 }
